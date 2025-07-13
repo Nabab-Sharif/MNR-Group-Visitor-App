12f7519dfcdf1840bd6d043680b753fff7cf0859
Nabab <mdnababsharif2@gmail.com>
2025-07-13 14:49:50 +0600

    Change button color in Form

--- src/components/visitor/VisitorFormActions.jsx
+++ src/components/visitor/VisitorFormActions.jsx
@@ -18,7 +18,7 @@ const VisitorFormActions = ({ onReset }) => {
     <div className="flex justify-end space-x-4 pt-4">
        <AlertDialog>
         <AlertDialogTrigger asChild>
-          <Button type="button" variant="outline" className="text-yellow-50 border-rose-700 bg-slate-600 hover:bg-rose-700 hover:text-black">
+          <Button type="button" variant="outline" className="text-yellow-50 border-rose-700 bg-slate-900 hover:bg-rose-700 hover:text-black">
             <Trash2 className="mr-2 h-4 w-4" /> Reset
           </Button>
         </AlertDialogTrigger>
@@ -37,7 +37,7 @@ const VisitorFormActions = ({ onReset }) => {
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
-      <Button type="submit" className="bg-gradient-to-r from-purple-500 to-pink-600 hover:from-purple-600 hover:to-pink-700 text-black font-semibold shadow-lg">
+      <Button type="submit" className="bg-gradient-to-r  from-purple-500 to-pink-600 hover:from-purple-600 hover:to-pink-700 text-black font-semibold shadow-lg">
         <Save className="mr-2 h-4 w-4" /> Save Data
       </Button>
     </div>
